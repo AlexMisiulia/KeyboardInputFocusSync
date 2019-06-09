37626050d2511eaf2dbd6b49114816436b3e4b46
Alexey Misiulia <alexeymisiulia@gmail.com>
2019-06-09 20:21:29 +0300

    add a project

---
+++ app/src/main/java/com/simplifire/keyboardhideshow/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.simplifire.keyboardhideshow
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.button)
+        button.setOnClickListener {
+            startActivity(Intent(this, DetailsActivity::class.java))
+        }
+    }
+}
