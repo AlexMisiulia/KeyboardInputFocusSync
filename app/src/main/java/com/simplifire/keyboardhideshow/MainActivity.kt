47c904acf7bdb771c753428533c556d35fa2a75e
Alexey Misiulia <alexeymisiulia@gmail.com>
2019-06-09 20:35:41 +0300

    show keyboard if activity has focus

--- app/src/main/java/com/simplifire/keyboardhideshow/MainActivity.kt
+++ app/src/main/java/com/simplifire/keyboardhideshow/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.simplifire.keyboardhideshow
 
+import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +17,26 @@ class MainActivity : AppCompatActivity() {
         val button: Button = findViewById(R.id.button)
         button.setOnClickListener {
             startActivity(Intent(this, DetailsActivity::class.java))
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        // show the keyboard if has focus
+        currentFocus?.let {
+            showSoftKeyboard(it)
+        }
+    }
+
+    private fun showSoftKeyboard(view: View) {
+        if (view.requestFocus()) {
+            val imm = view.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
+
+            // here is one more tricky issue
+            // imm.showSoftInputMethod doesn't work well
+            // and imm.toggleSoftInput(InputMethodManager.SHOW_IMPLICIT, 0) doesn't work well for all cases too
+            imm?.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0)
         }
     }
 }
